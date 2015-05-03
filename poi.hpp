04ea53818cc0d3ad0ecc4f7c59e5b8028ebb4c13
azwaradli <azwar.adli@gmail.com>
2015-05-03 22:55:05 +0700

    Update FIX

--- poi.hpp
+++ poi.hpp
@@ -1,6 +1,8 @@
 #ifndef POIFS_H
 #define POIFS_H
 
+extern POIFS POI;
+
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
@@ -21,47 +23,7 @@ using namespace std;
 class POIFS {
 public:
 	// inner class
-	class Entry{
-	public:
-		// ctor
-		Entry();
-		Entry(ushort enPos, char offs);
-
-		// entry attribute getter
-		char* getNama();
-		char getAttribut();
-		short getTime();
-		short getDate();
-		short getIndex();
-		int getSize();
-
-		// entry method for the block data
-		Entry getNextEntry();
-		Entry getEntryfromPath(const char* path);
-		Entry getEmptyEntry();
-		Entry getNewEntryfromPath(const char* path);
-
-		// entry attribute setter
-		void setNama(const char* nama);
-		void setAttribut(const char attribut);
-		void setTime(const short time);
-		void setDate(const short date);
-		void setIndex(const short index);
-		void setSize(const int size);
-		
-		// fungsi & method
-		bool isEmpty();
-		void makeEmpty();
-
-		time_t getEntryTime();
-		void setCurrentTime();	
-
-	private:
-		char blockEntry[ENTRY_SIZE];	/* the size of the entry block */
-		ushort entryPosition;			/* the position of the block */
-		char off;						/* offset of the block (from 0-15) */
-	};
-
+	
 	// Constructor & Destructor
 	POIFS();
 	~POIFS();
